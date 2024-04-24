e82adcde98fbfbc6508a1cb55fcd9c32e213ee8f
JoelFaldin <joelfaldin@gmail.com>
2024-04-23 20:48:26 -0400

    refactor: improving structure of main backend app

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,7 @@
-import express, { Express, Request, Response } from "express";
-import cors from 'cors'
+import app from './app'
+import PORT from './utils/config'
 
-const app = express();
-app.use(cors())
-
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).json({ message: "This message is from the backend" })
-})
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server up!')
-    console.log('http://localhost:3000')
+    console.log(`http://localhost:${PORT}`)
 })
