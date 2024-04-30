069d3de33d262a1e0713a11adfb158a4721a40bb
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2024-04-29 22:24:57 -0400

    feat: :sparkles: protected routes config

--- frontend/src/routes/protectedRoute.tsx
+++ frontend/src/routes/protectedRoute.tsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
-export const ProtectedRoute = () => {
-    return (
-      <>
-      <h1></h1>
-      </>
-    )
-  }
+interface ProtectedRouteProps {
+  roles?: string[];
+}
+
+function ProtectedRoute({ roles }: ProtectedRouteProps) {
+  const navigate = useNavigate();
+  const [userRole, setUserRole] = useState<string | null>(null);
+  const isAuthenticated = localStorage.getItem("token");
+  const userId = localStorage.getItem("userId");
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/");
+    } 
+  }, [isAuthenticated, userId, roles, userRole, navigate]);
+
+  return <Outlet />;
+}
+
+export default ProtectedRoute;
