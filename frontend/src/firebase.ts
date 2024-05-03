a737c46c1f81c6e97d2ad367b340608d8f294c6a
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2024-05-03 19:46:34 -0400

    feat: :sparkles: firebase initial config to hosting

---
+++ frontend/src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJSE12d6lXA1lCvkPKLcXcKh-5I9Ytmts",
+  authDomain: "blacksharkweb-frontend.firebaseapp.com",
+  projectId: "blacksharkweb-frontend",
+  storageBucket: "blacksharkweb-frontend.appspot.com",
+  messagingSenderId: "539547711779",
+  appId: "1:539547711779:web:fa111e3128dc344454b496"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
