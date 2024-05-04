34a6102586d7a72d6d0375622ddab59897a2a4ae
DiegoArayaT <nachox341@gmail.com>
2024-05-03 21:31:25 -0400

    Visual changes in login

--- frontend/src/auth/login.tsx
+++ frontend/src/auth/login.tsx
@@ -17,32 +17,34 @@ export const Login = () => {
           <div className="limit font-myriad-pro rounded-xl" >
               <form className="flex flex-col items-center max-w-400px mx-auto p-20 rounded-lg">
 
-                <span className="login-title mb-4">Iniciar Sesión</span>
+                <div>
+                  <h2 className="mt-5 text-2xl font-extrabold text-center text-neutral-600">Iniciar Sesión</h2>
+                </div>
                 
-                <div className="email flex flex-col items-start mb-4">
-                  <span className="label-email-input">Correo</span>
+                <div className="email flex flex-col items-start mb-4 pt-10">
+                  <span className="label-email-input py-1">Correo</span>
                   <div className="flex items-center">
-                    <FontAwesomeIcon icon={faUser}/>
-                    <input className="email-input ml-2" type="email" name="username" placeholder="Ingrese su correo" />
+                    <FontAwesomeIcon icon={faUser} className="px-4"/>
+                    <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="email" name="username" placeholder="Ingrese su correo" />
                   </div>
                 </div>
 
                 <div className="password flex flex-col items-start mb-4">
-                  <span className="label-input-password">Contraseña</span>
+                  <span className="label-input-password py-1">Contraseña</span>
                   <div className="flex items-center">
-                    <FontAwesomeIcon icon={faLock}/>
-                    <input className="password-input ml-2" type="password" name="pass" placeholder="Ingrese su contraseña"/>
+                    <FontAwesomeIcon icon={faLock} className="px-4"/>
+                    <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="password" name="pass" placeholder="Ingrese su contraseña"/>
                   </div>
                 </div>
 
-                <div className="mb-4">
+                <div className="mb-4 pt-5">
                   <Link to={'/signup'}>
-                    <a>¿Has olvidado tu contraseña?</a>
+                    <a href="#" className="font-medium text-blue-600 hover:text-blue-500 transition-colors duration-300">¿Has olvidado tu contraseña?</a>
                   </Link>
                 </div>
 
                 <div className="contenedor">
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  <button className="flex items-center justify-center w-full px-10 py-2.5 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                     Ingresar
                   </button>
                 </div>
--- frontend/src/auth/register.tsx
+++ frontend/src/auth/register.tsx
@@ -1,62 +1,73 @@
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Link } from "react-router-dom"
+import { faChevronLeft, faUser, faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 export const Register = () => {
-    return (
-      <>
-        <div>
-          <div className="imback fixed top-0 left-0 p-2">
-              <Link to={'/'}>
-                <span className="flex items-center justify-center rounded-full w-20 h-20">
-                  <FontAwesomeIcon icon={faChevronLeft} size="2xl"/>
-                </span>
-              </Link>
-          </div>
-  
-          <div className="flex justify-center items-center h-screen">
-              <form className="font-myriad-pro flex flex-col items-center max-w-xl mx-auto p-20 rounded-lg ">
-                  <div>
-                    <h2 className="mt-5 text-2xl font-extrabold text-center text-neutral-600">Registrarse</h2>
-                  </div>
-
-                  <div className="w-full pt-10">
-                    <span className="label-name-input">Nombre completo</span>
-                    <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="text" name="fullname" placeholder="Nombre completo"/>
-                  </div>
-    
-                  <div className="w-full pt-3">
-                    <span className="label-user-input">Correo</span>
-                    <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="email" name="username" placeholder="Ingrese su correo" />
-                    {/* icono de un usuario */}
-                  </div>
-    
-                  <div className="w-full pt-3">
-                    <span className="label-input-password">Contraseña</span>
-                    <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="password" name="pass" placeholder="Ingrese su contraseña"/>
-                    {/* icono de un candado */}
-                  </div>
-    
-                  <div className="w-full pt-3">
-                    <span className="label-input-reapetpassword">Confirma la contraseña</span>
-                    <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="password" name="repassword" placeholder="Confirme su contraseña"/>
-                    {/* icono de un candado */}
-                  </div>
-
-                  <div className="mb-4 pt-5">
-                    <Link to={'/login'}>
-                       <a href="#" className="font-medium text-blue-600 hover:text-blue-500">¿Ya tienes cuenta?</a>
-                     </Link>
-                  </div>
-    
-                  <div className="contenedor">
-                    <button className="flex items-center justify-center w-full px-10 py-2.5 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                         Registrarte
-                     </button>
-                  </div>
-              </form>
-          </div>
+  return (
+    <>
+      <div>
+        <div className="imback fixed top-0 left-0 p-2">
+          <Link to={"/"}>
+            <span className="flex items-center justify-center rounded-full w-20 h-20">
+              <FontAwesomeIcon icon={faChevronLeft} size="2xl" />
+            </span>
+          </Link>
         </div>
-      </>
-    )
-}
+
+        <div className="flex justify-center items-center h-screen w-full">
+          <form className="font-myriad-pro flex flex-col items-center max-w-sm w-full px-4 md:px-0 py-8 rounded-lg ">
+            <div>
+              <h2 className="mt-5 pb-10 text-2xl font-extrabold text-center text-neutral-600">Registrarse</h2>
+            </div>
+
+            <div className="w-full pt-1">
+              <span className="label-name-input">Nombre completo</span>
+              <div className="flex items-center py-2">
+                <FontAwesomeIcon icon={faUser} className="px-4"/>
+                <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="text" name="fullname" placeholder="Nombre completo"/>
+              </div>
+            </div>
+            <div className="w-full pt-1">
+              <span className="label-user-input">Correo</span>
+              <div className="flex items-center py-2">
+                <FontAwesomeIcon icon={faEnvelope} className="px-4"/>
+                <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="email" name="username" placeholder="Ingrese su correo" />
+                {/* icono de un usuario */}
+              </div>
+            </div>
+
+            <div className="w-full pt-1">
+              <span className="label-input-password">Contraseña</span>
+              <div className="flex items-center py-2">
+                <FontAwesomeIcon icon={faLock} className="px-4"/>
+                <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="password" name="pass" placeholder="Ingrese su contraseña"/>
+                {/* icono de un candado */}
+              </div>       
+            </div>
+
+            <div className="w-full pt-3">
+              <span className="label-input-reapetpassword">Confirma la contraseña</span>
+              <div className="flex items-center py-1">
+                <FontAwesomeIcon icon={faLock} className="px-4"/>
+                <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="password" name="pass" placeholder="Ingrese su contraseña"/>
+                {/* icono de un candado */}
+              </div> 
+            </div>
+
+            <div className="mb-4 pt-5">
+              <Link to={'/login'}>
+                  <a href="#" className="font-medium text-blue-600 hover:text-blue-500">¿Ya tienes cuenta?</a>
+                </Link>
+            </div>
+
+            <div className="contenedor">
+              <button className="flex items-center justify-center w-full px-10 py-2.5 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    Registrarte
+                </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
