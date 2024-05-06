ace9373e07cfa34dc67666e58decd80eccab540c
DiegoArayaT <nachox341@gmail.com>
2024-05-06 15:43:11 -0400

    refactor: updating register design

--- frontend/src/auth/login.tsx
+++ frontend/src/auth/login.tsx
@@ -1,4 +1,4 @@
-import { faChevronLeft, faLock, faUser } from "@fortawesome/free-solid-svg-icons"
+import { faChevronLeft} from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Link } from "react-router-dom"
 
--- frontend/src/auth/register.tsx
+++ frontend/src/auth/register.tsx
@@ -1,62 +1,51 @@
-import { faChevronLeft, faUser, faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 
 export const Register = () => {
   return (
     <>
-      <div>
+      <div className="bg-[url(/background-input-photo.jpg)] bg-cover bg-center w-full h-screen bg-no-repeat">
         <div className="imback fixed top-0 left-0 p-2">
           <Link to={"/"}>
-            <span className="flex items-center justify-center rounded-full w-20 h-20">
+            <span className="flex items-center justify-center rounded-full w-20 h-20 text-white">
               <FontAwesomeIcon icon={faChevronLeft} size="2xl" />
             </span>
           </Link>
         </div>
 
         <div className="flex justify-center items-center h-screen w-full">
-          <form className="font-myriad-pro flex flex-col items-center max-w-sm w-full px-4 md:px-0 py-8 rounded-lg">
+          <form className="font-myriad-pro flex flex-col items-center max-w-md w-full md:px-10 py-8 rounded-lg  bg-black bg-opacity-60">
             <div>
-              <h2 className="mt-5 pb-10 text-2xl font-extrabold text-center text-neutral-600">Registrarse</h2>
+              <h2 className="mt-5 pb-10 text-2xl font-extrabold text-center text-white">Registrarse</h2>
             </div>
 
-            <div className="w-full pt-1">
-              <span className="label-name-input">Nombre completo</span>
-              <div className="flex items-center py-2">
-                <FontAwesomeIcon icon={faUser} className="px-4"/>
-                <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="text" name="fullname" placeholder="Nombre completo"/>
+            <div className="name flex flex-col items-start mb-4 pt-3 text-white">
+              <div className="flex items-center">
+                <input className="w-full pl-5 pr-20 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="text" name="fullname" placeholder="Nombre completo"/>
               </div>
             </div>
-            <div className="w-full pt-1">
-              <span className="label-user-input">Correo</span>
-              <div className="flex items-center py-2">
-                <FontAwesomeIcon icon={faEnvelope} className="px-4"/>
-                <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="email" name="username" placeholder="Ingrese su correo" />
-                {/* icono de un usuario */}
+            <div className="email flex flex-col items-start mb-4 pt-3 text-white">
+              <div className="flex items-center">
+                <input className="w-full pl-5 pr-20 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="email" name="username" placeholder="Ingrese su correo" />
               </div>
             </div>
 
-            <div className="w-full pt-1">
-              <span className="label-input-password">Contraseña</span>
-              <div className="flex items-center py-2">
-                <FontAwesomeIcon icon={faLock} className="px-4"/>
-                <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="password" name="pass" placeholder="Ingrese su contraseña"/>
-                {/* icono de un candado */}
+            <div className="password flex flex-col items-start mb-4 pt-3 text-white">
+              <div className="flex items-center">
+                <input className="w-full pl-5 pr-20 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="password" name="pass" placeholder="Ingrese su contraseña"/>
               </div>       
             </div>
 
-            <div className="w-full pt-3">
-              <span className="label-input-reapetpassword">Confirma la contraseña</span>
-              <div className="flex items-center py-1">
-                <FontAwesomeIcon icon={faLock} className="px-4"/>
-                <input className="w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="password" name="pass" placeholder="Ingrese su contraseña"/>
-                {/* icono de un candado */}
+            <div className="repassword flex flex-col items-start mb-4 pt-3 text-white">
+              <div className="flex items-center">
+                <input className="w-full pl-5 pr-20 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="password" name="pass" placeholder="Ingrese su contraseña"/>
               </div> 
             </div>
 
             <div className="mb-4 pt-5">
               <Link to={'/login'}>
-                  <a href="#" className="font-medium text-blue-600 hover:text-blue-500">¿Ya tienes cuenta?</a>
+                  <a href="#" className="font-medium text-white hover:text-blue-500">¿Ya tienes cuenta?</a>
                 </Link>
             </div>
 
