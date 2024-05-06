c8253d0ce4bd7948b0b026c96488e7458a8ba2fe
DiegoArayaT <nachox341@gmail.com>
2024-05-06 14:20:04 -0400

    refactor: delete icon login

--- frontend/src/auth/login.tsx
+++ frontend/src/auth/login.tsx
@@ -15,23 +15,27 @@ export const Login = () => {
         </div>
 
         <div className="flex justify-center items-center h-screen pb-10">
-          <form className="font-myriad-pro flex flex-col items-center max-w-md w-full md:px-0 py-20 rounded-lg bg-black bg-opacity-60">
+          <form className="font-myriad-pro flex flex-col items-center max-w-md w-full md:px-0 pt-20 rounded-lg bg-black bg-opacity-60">
             <div>
               <h2 className=" text-2xl font-extrabold text-white">Iniciar Sesión</h2>
             </div>
             
             <div className="email flex flex-col items-start mb-4 pt-10 text-white">
               <div className="flex items-center">
-                <FontAwesomeIcon icon={faUser} className="px-4"/>
-                <input className="w-full px-10 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="email" name="username" placeholder="Ingrese su correo" />
+                <input className="w-full pl-5 pr-20 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="email" name="username" placeholder="Ingrese su correo" />
               </div>
             </div>
 
             <div className="password flex flex-col items-start mb-4 text-white pt-5">
               <div className="flex items-center">
-                <FontAwesomeIcon icon={faLock} className="px-4"/>
-                <input className="w-full px-10 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="password" name="pass" placeholder="Ingrese su contraseña"/>
+                <input className="w-full pl-5 pr-20 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" type="password" name="pass" placeholder="Ingrese su contraseña"/>
               </div>
+            </div>
+
+            <div className="contenedor pt-8">
+              <button className="flex items-center justify-center w-full px-20 py-2.5 text-xl font-large text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                Ingresar
+              </button>
             </div>
 
             <div className="mb-4 pt-5 text-white">
@@ -40,11 +44,12 @@ export const Login = () => {
               </Link>
             </div>
 
-            <div className="contenedor">
-              <button className="flex items-center justify-center w-full px-10 py-2.5 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                Ingresar
-              </button>
+            <div className="pt-10 pb-5 text-white">
+              <Link to={'/signup'}>
+                <a href="#" className="font-medium blue-bs hover:text-blue-500 transition-colors duration-300">¿No tienes una cuenta? Registrate</a>
+              </Link>
             </div>
+
           </form>         
         </div>
       </div>
