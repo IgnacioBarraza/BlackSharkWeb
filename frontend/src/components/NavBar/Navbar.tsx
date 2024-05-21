696e5a473a8389c26cc3ca8fa970481a876d80f0
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-05-21 03:09:19 -0400

    definition of the breakpoint for the navbar and creation of its style sheet

--- frontend/src/components/NavBar/Navbar.tsx
+++ frontend/src/components/NavBar/Navbar.tsx
@@ -2,33 +2,49 @@ import { faList, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import '../../styles/navbar.css'
 
 export const Navbar = () => {
-  const [isSideMenuOpen, setMenu] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <>
-        <div className="flex items-center justify-center">
-            <div className="flex items-center space-x-16 ">
-                <Link to={'/servicios'}>   
-                <span className="font-myriad-pro font-medium transition duration-500 transform hover:scale-110 px-2 mr-7 text-2xl flex items-center justify-center">Servicios</span>
-                </Link>
-                <Link to={'/gallery'} className="font-myriad-pro font-medium transition duration-500 transform hover:scale-110 text-2xl px-2 mr-7 flex items-center justify-center">
-                <span>Galería</span>
-                </Link>
-                <Link to={'/'}>
-                <div className="w-28 h-28 z-100">
-                <img src="/BlackShark.png" alt="Blackshark logo" />
-                </div>
-                </Link>
-                <Link to={'/contact'} className="font-myriad-pro font-medium  transition duration-500 transform hover:scale-110 text-2xl px-2 mr-7 flex items-center justify-center">
-                <span >Contacto</span>
-                </Link>
-                <Link to={'/login'} className="font-myriad-pro font-medium  transition duration-500 transform hover:scale-110 text-2xl px-2 mr-7 flex items-center justify-center">
-                  <span>Iniciar Sesión</span>
-                </Link>
-            </div>
+      <div className="flex items-center relative justify-between px-4 py-2">
+        <div className="flex items-center space-x-16 md:space-x-16">
+              <Link to={'/servicios'}>   
+              <span className="font-myriad-pro font-medium transition duration-500 transform hover:scale-110 px-2 mr-7 text-2xl items-center justify-center hidden md:flex">Servicios</span>
+              </Link>
+              <Link to={'/gallery'} className="font-myriad-pro font-medium transition duration-500 transform hover:scale-110 text-2xl px-2 mr-7 items-center justify-center hidden md:flex">
+              <span>Galería</span>
+              </Link>
+              <Link to={'/'}>
+              <div className="w-28 h-28 z-100 md:justify-center">
+              <img src="/BlackShark.png" alt="Blackshark logo" />
+              </div>
+              </Link>
+              <Link to={'/contact'} className="font-myriad-pro font-medium  transition duration-500 transform hover:scale-110 text-2xl px-2 mr-7 items-center justify-center hidden md:flex">
+              <span >Contacto</span>
+              </Link>
+              <Link to={'/login'} className="font-myriad-pro font-medium  transition duration-500 transform hover:scale-110 text-2xl px-2 mr-7 items-center justify-center hidden md:flex">
+                <span>Iniciar Sesión</span>
+              </Link>
         </div>
+        <button id="menu-button" className="block md:hidden p-2 ml-auto" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faList} style={{color: "#000000"}} size="2xl"/>
+        </button>
+      </div>
+      {isOpen && (
+        <div className="absolute top-full right-0 mt-2 w-full bg-white shadow-lg md:hidden">
+          <Link to={'/servicios'} className="font-myriad-pro block px-4 py-2 text-black hover:bg-gray-200">Servicios</Link>
+          <Link to={'/gallery'} className="font-myriad-pro block px-4 py-2 text-black hover:bg-gray-200">Galería</Link>
+          <Link to={'/contact'} className="font-myriad-pro block px-4 py-2 text-black hover:bg-gray-200">Contacto</Link>
+          <Link to={'/login'} className="font-myriad-pro block px-4 py-2 text-black hover:bg-gray-200">Iniciar Sesión</Link>
+        </div>
+      )}
     </>
   )
 }
