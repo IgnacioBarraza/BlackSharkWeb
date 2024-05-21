5dc2767e750119698fd52e223eddd29a185d809f
JoelFaldin <joelfaldin@gmail.com>
2024-05-21 12:15:52 -0400

    refactor: improving limiters timer

--- backend/utils/rate-limiter.ts
+++ backend/utils/rate-limiter.ts
@@ -1,8 +1,8 @@
 import { rateLimit } from 'express-rate-limit'
 
 export const rateLimiter = rateLimit({
-    windowMs: 1 * 60 * 1000,
-    limit: 20,
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
     standardHeaders: true,
     legacyHeaders: false
 })
--- backend/utils/slow-down.ts
+++ backend/utils/slow-down.ts
@@ -1,7 +1,7 @@
 import { slowDown } from 'express-slow-down'
 
 export const slowDownLimiter = slowDown({
-    windowMs: 1 * 60 * 1000,
-    delayAfter: 10,
+    windowMs: 15 * 60 * 1000,
+    delayAfter: 50,
     delayMs: hits => hits * 100
 })
