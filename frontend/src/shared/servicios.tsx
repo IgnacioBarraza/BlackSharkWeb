76eaf5f8fe14e68cc6eb211b745bff235630d7eb
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-05-25 23:21:55 -0400

    services interface identation fix

--- frontend/src/shared/servicios.tsx
+++ frontend/src/shared/servicios.tsx
@@ -6,11 +6,12 @@ export const Servicios = () => {
     <div className="bg-white bg-cover bg-center w-full h-screen bg-no-repeat flex flex-col">
        <div className="flex-grow flex items-center justify-center pb-10">
         <div className="w-full flex justify-center items-center py-1 bg-transparent z-100 border-b border-gray-300">
-        <Navbar/>
+            <Navbar/>
         </div>
       </div>
       <div className="mx-auto mt-8 mb-20">
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 ml-24 mr-24">
+
             <div className="bg-white rounded-lg shadow-lg transition-shadow hover:shadow-xl">
                 <img src="/servicio1.jpg" alt="servicio1" className="w-full h-64 object-cover "/>
                 <div className="p-4">
@@ -18,6 +19,7 @@ export const Servicios = () => {
                     <p className="text-gray-700">Descripcion del servicio</p>
                 </div>
             </div>
+
             <div className="bg-white rounded-lg shadow-lg transition-shadow hover:shadow-xl">
                 <img src="/servicio1.jpg" alt="servicio1" className="w-full h-64 object-cover "/>
                 <div className="p-4">
@@ -25,6 +27,7 @@ export const Servicios = () => {
                     <p className="text-gray-700">Descripcion del servicio</p>
                 </div>
             </div>
+
             <div className="bg-white rounded-lg shadow-lg transition-shadow hover:shadow-xl">
                 <img src="/servicio1.jpg" alt="servicio1" className="w-full h-64 object-cover "/>
                 <div className="p-4">
@@ -32,20 +35,23 @@ export const Servicios = () => {
                     <p className="text-gray-700">Descripcion del servicio</p>
                 </div>
             </div>
+
             <div className="bg-white rounded-lg shadow-lg transition-shadow hover:shadow-xl">
                 <img src="/servicio1.jpg" alt="servicio1" className="w-full h-64 object-cover "/>
                 <div className="p-4">
                     <h2 className="text-lg font-bold mb-2">Servicio 4</h2>
                     <p className="text-gray-700">Descripcion del servicio</p>
                 </div>
-            </div>            
+            </div>   
+
             <div className="bg-white rounded-lg shadow-lg transition-shadow hover:shadow-xl">
                 <img src="/servicio1.jpg" alt="servicio1" className="w-full h-64 object-cover "/>
                 <div className="p-4">
                     <h2 className="text-lg font-bold mb-2">Servicio 5</h2>
                     <p className="text-gray-700">Descripcion del servicio</p>
                 </div>
-            </div>            
+            </div>  
+
             <div className="bg-white rounded-lg shadow-lg transition-shadow hover:shadow-xl">
                 <img src="/servicio1.jpg" alt="servicio1" className="w-full h-64 object-cover "/>
                 <div className="p-4">
@@ -53,6 +59,7 @@ export const Servicios = () => {
                     <p className="text-gray-700">Descripcion del servicio</p>
                 </div>
             </div>
+
             <div className="bg-white rounded-lg shadow-lg transition-shadow hover:shadow-xl">
                 <img src="/servicio1.jpg" alt="servicio1" className="w-full h-64 object-cover "/>
                 <div className="p-4">
@@ -60,6 +67,7 @@ export const Servicios = () => {
                     <p className="text-gray-700">Descripcion del servicio</p>
                 </div>
             </div>
+
             <div className="bg-white rounded-lg shadow-lg transition-shadow hover:shadow-xl">
                 <img src="/servicio1.jpg" alt="servicio1" className="w-full h-64 object-cover "/>
                 <div className="p-4">
@@ -67,6 +75,7 @@ export const Servicios = () => {
                     <p className="text-gray-700">Descripcion del servicio</p>
                 </div>
             </div>
+
             <div className="bg-white rounded-lg shadow-lg transition-shadow hover:shadow-xl">
                 <img src="/servicio1.jpg" alt="servicio1" className="w-full h-64 object-cover "/>
                 <div className="p-4">
@@ -74,6 +83,7 @@ export const Servicios = () => {
                     <p className="text-gray-700">Descripcion del servicio</p>
                 </div>
             </div>
+            
         </div>
       </div>
     </div>
