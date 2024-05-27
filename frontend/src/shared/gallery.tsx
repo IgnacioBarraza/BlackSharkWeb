c25ba23a902f1deb13e66df997545cee37ee4c6f
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-05-27 00:29:41 -0400

    import of the icon to use and the useContext hook

--- frontend/src/shared/gallery.tsx
+++ frontend/src/shared/gallery.tsx
@@ -1,6 +1,14 @@
 import { Navbar } from "../components/NavBar/Navbar";
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { UserContext } from "../providers/userContext";
+import { useContext } from "react";
+
 
 export const Gallery = () => {
+
+  const { userType, userToken } = useContext(UserContext);
+
   return (
     <>
     <div className="bg-white bg-cover bg-center w-full h-screen bg-no-repeat flex flex-col overflow-y-auto">
@@ -11,6 +19,16 @@ export const Gallery = () => {
       </div>
       <div className=" p-5 md:p-10">
         <div className="columns-1 gap-5 lg:gap-8 sm:columns-2 lg:columns-3 xl:columns-4 [&>img:not(:first-child)]:mt-5 lg:[&>img:not(:first-child)]:mt-8">
+          {userType === "admin" &&  userToken &&(
+            <>
+            <div className="w-full flex flex-col items-center justify-center">
+              <button className="w-full h-full flex items-center justify-center rounded-lg shadow-md border border-gray-300 transition duration-300 ease-in-out transform hover:scale-105">
+                <FontAwesomeIcon icon={faPlus} style={{color: "#000000",}} size="10x"/>
+                <h1 className="mt-2 text-center">Agregar Imagen</h1>
+              </button>
+            </div>
+            </>
+          )}
           <img src="/paisaje1.jpg" alt="" className="rounded-lg shadow-md border border-gray-300 transition duration-300 ease-in-out transform hover:scale-105"/>
           <img src="/paisaje1.jpg" alt="" className="rounded-lg shadow-md border border-gray-300 transition duration-300 ease-in-out transform hover:scale-105"/>
           <img src="/paisaje1.jpg" alt="" className="rounded-lg shadow-md border border-gray-300 transition duration-300 ease-in-out transform hover:scale-105"/>
