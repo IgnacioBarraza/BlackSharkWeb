1e89d6e3a265663fcb9f42a3969f3e23a1b539a8
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-05-29 19:31:38 -0400

    interface and modal responsive

--- frontend/src/shared/servicios.tsx
+++ frontend/src/shared/servicios.tsx
@@ -113,8 +113,8 @@ export const Servicios = () => {
             )} 
 
             {showInterface && (
-            <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
-              <div className="bg-white rounded-lg shadow-lg p-6 w-96">
+            <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 sm:px-0">
+              <div className="bg-white rounded-lg shadow-lg p-6 w-96 max-w-md">
                 <h2 className="font-myriad-pro text-xl font-bold mb-4 text-center">Agregar Nuevo Servicio</h2>
                 <form action="#" method="POST" className="">
                   <div>
