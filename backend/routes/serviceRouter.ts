3101d0243bdb8c57b1f8962e3d4b3f084d73b70b
JoelFaldin <joelfaldin@gmail.com>
2024-05-30 00:49:22 -0400

    feat: adding router to create a new service

--- backend/routes/serviceRouter.ts
+++ backend/routes/serviceRouter.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { randomUUID } from 'crypto'
 
 import { connect } from '../utils/db'
 
@@ -17,6 +18,26 @@ serviceRouter.get('/', async (req, res) => {
         if (connection) {
             connection.end()
         }
+    }
+})
+
+serviceRouter.post('/new', async (req, res) => {
+    const connection = connect()
+    const body = req.body
+
+    try {
+        const newService = {
+            id_servicios: randomUUID(),
+            nombre: body.nombre,
+            precio: body.precio,
+            descripcion: body.descripcion
+        }
+
+        await connection.query(`INSERT INTO servicios (id_servicios, nombre, precio, descripcion) VALUES (?, ?, ?, ?)`, [newService.id_servicios, newService.nombre, newService.precio, newService.descripcion])
+        return res.status(201).json({ message: 'Servicio creado!' })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: 'Hubo un error en el servidor al intentar guardar el servicio. Inténtalo más tarde.' })
     }
 })
 
