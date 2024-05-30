d16d9af430d85c7fe618ad66585e00952a12a4f5
DiegoArayaT <nachox341@gmail.com>
2024-05-30 16:10:23 -0400

    feat: When you press out the photo when you are in the window the entire photo closes

--- frontend/src/shared/gallery/imagemodal.tsx
+++ frontend/src/shared/gallery/imagemodal.tsx
@@ -3,18 +3,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 const ImageModal = ({ src, onClose }) => {
+    const handleClickOutside = (event) => {
+        if (event.target.classList.contains('modal-container')) {
+          onClose();
+        }
+      };
+
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75 p-4">
-            <div className="relative max-w-full max-h-full">
-                <img src={src} alt="" className="max-w-full max-h-screen object-contain" />
-                <FontAwesomeIcon
-                    icon={faCircleXmark}
-                    className="absolute top-4 right-4 text-white text-4xl hover:animate-beat-fade"
-                    onClick={onClose}
-                />
-            </div>
-        </div>
-    );
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75 p-4 modal-container"
+      onClick={handleClickOutside}
+    >
+      <div className="relative max-w-full max-h-full">
+        <img src={src} alt="" className="max-w-full max-h-screen object-contain" />
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          className="absolute top-4 right-4 text-white text-4xl hover:animate-beat-fade"
+          onClick={onClose}
+        />
+      </div>
+    </div>
+  );
 };
 
 
