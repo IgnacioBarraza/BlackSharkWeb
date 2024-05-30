b044965f970584c21a73010c3aa96e4169f34e65
DiegoArayaT <nachox341@gmail.com>
2024-05-30 17:04:21 -0400

    feat: when click out of the service, the service close

--- frontend/src/shared/servicios.tsx
+++ frontend/src/shared/servicios.tsx
@@ -81,6 +81,12 @@ export const Servicios = () => {
   const handleInterface = () => {
     setShowInterface(prevState => !prevState);
   };
+
+  const handleClickOutside = (event) => {
+    if (event.target.classList.contains('modal-container')) {
+      handleCloseModal();
+    }
+  };
   
   return (
     <>
@@ -183,19 +189,16 @@ export const Servicios = () => {
         </div>
 
         {selectedServicio && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div className="bg-white w-11/12 md:w-3/4 lg:w-2/3 h-3/4 md:h-auto rounded-lg p-4 md:p-8 flex flex-col md:flex-row overflow-y-auto">
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 modal-container" onClick={handleClickOutside}>
+            <div className=" bg-white w-11/12 md:w-3/4 lg:w-2/3 h-3/4 md:h-auto rounded-lg p-4 md:p-8 flex flex-col md:flex-row overflow-y-auto" >
               <div className="w-full md:w-1/2 h-64 md:h-auto flex items-center justify-center">
-                <div>
-
-                </div>
                 <img
                   src={selectedServicio.imgSrc}
                   alt={selectedServicio.title}
                   className="w-full h-full object-cover rounded-lg"
                 />
               </div>
-              <div className="md:w-1/2 p-4 md:p-8">
+              <div className="md:w-1/2 p-4 md:p-8 ">
                 <h2 className="text-2xl font-bold mb-4">{selectedServicio.title}</h2>
                 <p>{selectedServicio.description}</p>
                 <button
