765216ad6787d327215b0dca318ec559fda24cec
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-05-29 23:52:26 -0400

    a more specific class is updated

--- frontend/src/shared/gallery.tsx
+++ frontend/src/shared/gallery.tsx
@@ -32,7 +32,7 @@ export const Gallery = () => {
 
           {userType === "admin" &&  userToken &&(
             <>
-              <button  onClick={handleButtonClick} className="w-full height flex p-4 sm:p-6 md:p-8 lg:p-10 items-center justify-center bg-transparent hover:bg-gray-200 py-2 px-4 border border-gray-200 hover:border-transparent rounded transition duration-300 ease-in-out transform hover:scale-105">
+              <button  onClick={handleButtonClick} className="w-full height-button-admin flex p-4 sm:p-6 md:p-8 lg:p-10 items-center justify-center bg-transparent hover:bg-gray-200 py-2 px-4 border border-gray-200 hover:border-transparent rounded transition duration-300 ease-in-out transform hover:scale-105">
                 <div className="flex flex-col items-center justify-center space-y-4">
                   <FontAwesomeIcon icon={faPlus} style={{color: "#000000",}} size="4x"/>
                   <h1 className="font-myriad-pro font-light text-xl mt-2 text-center">Agregar Nueva Imagen</h1>
