6d29e59fff80c35dd58e3a3284261e0ef7f10137
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-05-31 21:43:58 -0400

    a new section is added to the home page

--- frontend/src/shared/homepage.tsx
+++ frontend/src/shared/homepage.tsx
@@ -17,16 +17,25 @@ export const Homepage = () => {
         <Navbar/>
       </div>
       <div className=" bg-[url(/cielo5.jpg)] bg-cover bg-center bg-no-repeat w-full h-screen">
-        <div className="flex justify-center items-center">
-          <div className="grid grid-cols-2 space-x-12">
-            <div className="flex justify-center">
-              <img className="w-128 h-128 fade-in" src="/BlackShark.png" alt="logo"/>
+        <div className="flex justify-center items-center h-full ">
+            <div className="flex flex-col justify-center space-y-8 items-center">
+              <h1 className=" text-center font-myriad-pro text-white font-bold font-size-100 block text-7xl fade-in">Bienvenido a BlackShark Studios </h1>
+              <p className="text-center font-myriad-pro text-white text-3xl font-medium fade-in w-1/2">En BlackShark Studios, nos especializamos en potenciar la imagen de tu marca a través de nuestros servicios integrales de fotografía, diseño gráfico y marketing digital.</p>
             </div>
-            <div className="flex flex-col justify-center space-y-8">
-              <h1 className=" text-center font-myriad-pro text-white font-bold font-size-100 block text-5xl fade-in">Welcome to BlackShark Studios </h1>
-              <p className="text-center font-myriad-pro text-white text-2xl font-medium fade-in">En BlackShark Studios, nos especializamos en potenciar la imagen de tu marca a través de nuestros servicios integrales de fotografía, diseño gráfico y marketing digital.</p>
-            </div>
-          </div>
+        </div>
+      </div>
+      <div className="bg-white bg-cover bg-center bg-no-repeat w-full h-screen">
+        <header className="bsw-background py-16">
+          <h1 className="font-myriad-pro text-3xl text-center text-white font-bold">Servicios Principales</h1>
+        </header>
+        <div>
+          Servicio 1
+        </div>
+        <div>
+          Servicio 2
+        </div>
+        <div>
+          Servicios
         </div>
       </div>
       </>
