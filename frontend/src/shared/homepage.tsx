34b8ec6907df0873ab38de89fdf23b1b02b683de
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-06-12 00:21:09 -0400

    latest changes

--- frontend/src/shared/homepage.tsx
+++ frontend/src/shared/homepage.tsx
@@ -8,15 +8,15 @@ export const Homepage = () => {
       <div className="flex justify-center items-center bg-transparent">
         <Navbar/>
       </div>
-      <div className=" bg-[url(/cielo5.jpg)] bg-cover bg-center bg-no-repeat w-full h-screen">
-        <div className="flex justify-center items-center h-full ">
+      <div className=" bg-[url(/cielo5.jpg)] bg-cover bg-center bg-no-repeat w-full h-screen overflow-hidden overflow-y-auto">
+        <div className="flex justify-center items-center h-full">
             <div className="flex flex-col justify-center space-y-8 items-center">
-              <h1 className=" text-center font-myriad-pro text-white font-bold font-size-100 block text-7xl fade-in">Bienvenido a Black Shark Studios </h1>
-              <p className="text-center font-myriad-pro text-white text-3xl font-medium fade-in w-1/2">Nos especializamos en potenciar la imagen de tu marca a través de nuestros servicios integrales de fotografía, diseño gráfico y marketing digital.</p>
+              <h1 className=" text-center font-myriad-pro text-white font-bold font-size-100 block text-5xl fade-in sm:text-3xl md:text-4xl lg:text-5xl">Bienvenido a Black Shark Studios </h1>
+              <p className="text-center font-myriad-pro text-white text-3xl font-medium fade-in w-1/2 sm:text-2xl md:text-2xl lg:text-3xl">Nos especializamos en potenciar la imagen de tu marca a través de nuestros servicios integrales de fotografía, diseño gráfico y marketing digital.</p>
             </div>
         </div>
       </div>
-      <Footer />
+      <Footer/>
       </>
     )
   }
