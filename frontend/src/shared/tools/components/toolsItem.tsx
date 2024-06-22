219823e3ce058066db5644bb7ea3fd521c9e67d7
JoelFaldin <joelfaldin@gmail.com>
2024-06-22 01:06:18 -0400

    feat: adding functionality to update tool name

--- frontend/src/shared/tools/components/toolsItem.tsx
+++ frontend/src/shared/tools/components/toolsItem.tsx
@@ -1,26 +1,64 @@
-import { faImage } from '@fortawesome/free-solid-svg-icons';
+import { faBan, faCheck, faImage, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useProps } from '../../../hooks/useProps';
+import { useState } from 'react';
+import { UpdateEquipment } from '../../../utils/interfaces';
 
-export const ToolsItem = ({ tool, onRemove }) => {
+export const ToolsItem = ({ tool, onRemove, onUpdate }) => {
     const { userType, userToken } = useProps();
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editName, setEditName] = useState(tool.nombre_equipo);
+    const [editToolType, setEditToolType] = useState(tool.tipo_eqiupo)
+
+    const saveUpdate = async () => {
+      const updatedTool: UpdateEquipment = {
+        nombre_equipo: editName ?? tool.nombre_equipo,
+        tipo_equipo: editToolType ?? tool.tipo_equipo
+      }
+      onUpdate(tool.id_equipo, updatedTool);
+      setIsEditing(prev => !prev);
+    }
+
+    const cancelUpdate = () => {
+      setIsEditing(prev => !prev);
+      setEditName(tool.nombre_equipo);
+    }
 
     return (
         <>
             <div className="flex items-center justify-between p-4 bg-white rounded shadow mb-4">
                 <div className="flex items-center">
                     <FontAwesomeIcon icon={faImage} className="mr-4" size='2x'/>
-                    <span>{tool.nombre_equipo}</span>
+                    {isEditing ? (
+                        <input value={editName} onChange={(event) => setEditName(event.target.value)} className="border-2 border-black w-fit" />
+                    ) : (
+                        <span>{tool.nombre_equipo}</span>
+                    )}
                 </div>
                 {userType === "admin" &&  userToken && (
-                <>
-                    <div className="flex items-center">
-                        <button onClick={onRemove} className="text-red-500">
-                            <FontAwesomeIcon icon={faTimes} />
-                        </button>
-                    </div>
-                </>
+                    <div className="flex items-center gap-x-2">
+                    {isEditing ? (
+                        <>
+                            <button onClick={saveUpdate}>
+                                <FontAwesomeIcon icon={faCheck} />
+                            </button>
+                            <button onClick={cancelUpdate}>
+                                <FontAwesomeIcon icon={faBan} />
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <button onClick={() => setIsEditing(prev => !prev)}>
+                                <FontAwesomeIcon icon={faPenToSquare} />
+                            </button>
+                            <button onClick={onRemove} className="text-red-500">
+                                <FontAwesomeIcon icon={faTimes} />
+                            </button>
+                        </>
+                    )}
+                </div>
                 )}
             </div>
         </>
