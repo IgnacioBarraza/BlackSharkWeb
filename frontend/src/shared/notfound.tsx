917266da8a71462debf2f131e64c1ca90a5dbded
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-06-23 00:43:08 -0400

    an image is added for the interface

--- frontend/src/shared/notfound.tsx
+++ frontend/src/shared/notfound.tsx
@@ -1,8 +1,19 @@
+import { Navbar } from "../components/NavBar/Navbar";
 
 export const Notfound = () => {
   return (
     <>
-    <h1>Error Not found</h1>
+    <div className="bg-white bg-cover bg-center w-full h-screen bg-no-repeat">
+      <Navbar/>
+      <div>
+        <div className="w-28 h-28">
+          <img src="/NotFound.jpg" alt="ErrorImage" />
+        </div>
+        <h1>404</h1>
+        <p>Error 404 - Page Not Found</p>
+        <p>La página solicitada no se pudo encontrar</p>
+      </div>
+    </div>
     </>
   )
 }
