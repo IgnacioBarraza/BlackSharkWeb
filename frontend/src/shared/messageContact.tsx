3a23133314c6536e18437844cf3a030d28ae2d97
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-07-03 10:24:16 -0400

    interface design to receive messages

--- frontend/src/shared/messageContact.tsx
+++ frontend/src/shared/messageContact.tsx
@@ -2,7 +2,29 @@
 export const MessageContact = () => {
   return (
     <>
-    <div>messageContact</div>
+    <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+        <h1 className="text-2xl font-bold mb-6">Mensajes</h1>
+        <div className="mb-4">
+            <div className="block text-sm font-bold mb-2">Nombre</div>
+            <p className="text-base">John</p>
+        </div>
+        <div className="mb-4">
+            <div className="block text-sm font-bold mb-2">Apellido</div>
+            <p className="text-base">Doe</p>
+        </div>
+        <div className="mb-4">
+            <div className="block text-sm font-bold mb-2">Email</div>
+            <p className="text-base">john.doe@example.com</p>
+        </div>
+        <div className="mb-4">
+            <div className="block text-sm font-bold mb-2">Teléfono</div>
+            <p className="text-base">971234587</p>
+        </div>
+        <div className="mb-4">
+            <div className="block text-sm font-bold mb-2">Mensaje</div>
+            <p className="text-base">Quiero trabajar contigo</p>
+        </div>
+    </div>
     </>
   )
 }
