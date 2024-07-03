aa92077c02ca3166bb30cf85a015c500abc67496
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-07-03 19:37:40 -0400

    import navbar component and fix design error

--- frontend/src/shared/messageContact.tsx
+++ frontend/src/shared/messageContact.tsx
@@ -1,30 +1,42 @@
+import { Navbar } from "../components/NavBar/Navbar"
 
 export const MessageContact = () => {
   return (
     <>
-    <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-6">Mensajes</h1>
-        <div className="mb-4">
-            <div className="block text-sm font-bold mb-2">Nombre</div>
-            <p className="text-base">John</p>
+        <div className="bg-white bg-cover bg-center w-full flex-grow bg-no-repeat flex flex-col">
+            <div className="flex-grow flex items-center justify-center">
+            <div className="w-full flex justify-center items-center py-1 bg-transparent z-100 border-b border-gray-300">
+                <Navbar/>
+            </div>
+            </div>
+        </div>   
+        <div className="bg-blue-strong-bs bg-cover bg-center w-full h-screen bg-no-repeat flex flex-col overflow-hidden overflow-y-automx-auto p-8 ">
+            <div className="flex flex-wrap -mx-4 items-center justify-center">
+                <div className="bg-white p-8 rounded-lg w-full max-w-md mx-4 mb-4">
+                    <h1 className="font-myriad-pro text-2xl font-bold mb-6 text-center">Mensajes</h1>
+                    <div className="mb-4">
+                        <div className="font-myriad-pro block text-sm font-bold mb-2">Nombre</div>
+                        <p className="text-base">John</p>
+                    </div>
+                    <div className="mb-4">
+                        <div className="font-myriad-pro block text-sm font-bold mb-2">Apellido</div>
+                        <p className="font-myriad-pro text-base">Doe</p>
+                    </div>
+                    <div className="mb-4">
+                        <div className="font-myriad-pro block text-sm font-bold mb-2">Email</div>
+                        <p className="font-myriad-pro text-base">john.doe@example.com</p>
+                    </div>
+                    <div className="mb-4">
+                        <div className="font-myriad-pro block text-sm font-bold mb-2">Teléfono</div>
+                        <p className="font-myriad-pro text-base">971234587</p>
+                    </div>
+                    <div className="mb-4">
+                        <div className="font-myriad-pro block text-sm font-bold mb-2">Mensaje</div>
+                        <p className="font-myriad-pro text-base">Quiero trabajar contigo</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="mb-4">
-            <div className="block text-sm font-bold mb-2">Apellido</div>
-            <p className="text-base">Doe</p>
-        </div>
-        <div className="mb-4">
-            <div className="block text-sm font-bold mb-2">Email</div>
-            <p className="text-base">john.doe@example.com</p>
-        </div>
-        <div className="mb-4">
-            <div className="block text-sm font-bold mb-2">Teléfono</div>
-            <p className="text-base">971234587</p>
-        </div>
-        <div className="mb-4">
-            <div className="block text-sm font-bold mb-2">Mensaje</div>
-            <p className="text-base">Quiero trabajar contigo</p>
-        </div>
-    </div>
     </>
   )
 }
