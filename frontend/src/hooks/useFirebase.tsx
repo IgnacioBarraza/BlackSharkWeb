ab11b4b3037db0dca4665b68329fba83df141777
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2024-09-07 23:27:14 -0400

    🎉 Migration to nextjs

--- frontend/src/hooks/useFirebase.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react"
-import { FirebaseContext } from "../providers/firebaseContext"
-
-export const useFirebase = () => {
-  const context = useContext(FirebaseContext)
-  if (!context) throw new Error('There is not firebase provider to use')
-  return context
-}
