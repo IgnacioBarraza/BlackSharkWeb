e3efa7cbe190693150f0b1ab8e51663ffcb3a6be
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2024-09-07 23:31:53 -0400

    🔥 Delete old projects

--- frontend/src/hooks/useAuth.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react"
-import { AuthContext } from "../providers/authContext"
-
-export const useAuth = () => {
-  const context = useContext(AuthContext)
-  if (!context) throw new Error('There is not auth provider to use')
-  return context
-}
