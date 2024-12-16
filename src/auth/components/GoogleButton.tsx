bf990619802f8f25fc16eb9cee78e88215ff0d46
JoelFaldin <joelfaldin@gmail.com>
2024-12-15 22:38:02 -0300

    feat: implementing full oauth login function

--- src/auth/components/GoogleButton.tsx
+++ src/auth/components/GoogleButton.tsx
@@ -1,15 +1,45 @@
 import { useGoogleLogin } from "@react-oauth/google"
+import { useNavigate } from "react-router-dom"
+import { useToast } from "@chakra-ui/react"
 import axios from "axios"
 
 import { useAuth } from "../../hooks/useAuth"
+import { useProps } from "@/hooks/useProps"
 import GoogleIcon from "@logos/Google"
 
 const GoogleOAuth = () => {
   const { oauth } = useAuth()
+  const { loginData } = useProps()
+  const navigate = useNavigate()
+  const toast = useToast()
 
   const googleSignIn = useGoogleLogin({
     onSuccess: async (response) => {
-      console.log(response)
+      try {
+        const res = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+          headers: {
+            Authorization: `Bearer ${response.access_token}`
+          }
+        })
+
+        const { data } = await oauth(res.data.email, res.data.name)
+        loginData(data.token, data.tipo_user, data.username, data.user_id, data.method)
+        navigate("/")
+
+        toast({
+          title: data.message,
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        })
+      } catch (error) {
+        toast({
+          title: error.response.data.message,
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        })
+      }
     }
   })
 
