324c55eb3c9f4a79063fe1d303f8f15c57d84515
jeri <jerimonski@gmail.com>
2024-12-29 17:23:27 -0300

    refactor:refactor footer design

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,44 +1,90 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { useLocation } from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Mail, MapPin, Phone } from "lucide-react";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { useLocation } from "react-router-dom";
 
 export const Footer = () => {
-  const location = useLocation()
-  const hideNavbarAndFooter = ['/auth', '/auth/signup', '/404'].some((path) =>
+  const location = useLocation();
+  const hideNavbarAndFooter = ["/auth", "/auth/signup", "/404"].some((path) =>
     location.pathname.startsWith(path)
-  )
+  );
 
   if (hideNavbarAndFooter) {
-    return null
+    return null;
   }
 
   return (
     <footer className="bg-black text-white py-6">
-      <div className="container mx-auto flex flex-col items-center">
-        <div className="flex items-center space-x-4 mb-4 relative w-full justify-center">
-          <div className="absolute left-0 top-1/2 transform -translate-y-1/2 w-1/3 md:w-2/5 border-t border-t-1 border-white" />
-          <a
-            href="https://www.facebook.com/ElLokojara"
-            aria-label="Facebook"
-            className="text-white hover:text-gray-400"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon={faFacebook} size="2x" />
-          </a>
-          <a
-            href="https://www.instagram.com/blackshark.studios/"
-            aria-label="Instagram"
-            className="text-white hover:text-gray-400"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon={faInstagram} size="2x" />
-          </a>
-          <div className="absolute right-0 top-1/2 transform -translate-y-1/2 w-1/3 md:w-2/5 border-t border-white" />
+      <div className=" grid grid-cols-1 md:grid-cols-4 p-4 gap-4">
+        {/* col 1 info */}
+        <div className="space-y-4 md:mx-auto">
+          <h3 className="text-xl font-semibold">Black Shark Studios</h3>
+          <p className="text-lg text-gray-light">
+            Soluciones creativas de marketing digital para hacer crecer tu
+            negocio
+          </p>
         </div>
-        <div className="text-center text-2xl md:text-3xl lg:text-4xl">
-          Black Shark Studios
+
+        {/* col 2 links*/}
+        <div className="space-y-4 md:mx-auto">
+          <h4 className="text-xl font-semibold">Atención al Cliente</h4>
+          <p className="text-lg text-gray-light">24/7 vía correo electrónico</p>
+        </div>
+
+        {/* col 3 Contact*/}
+        <div className="space-y-4 list-none md:mx-auto">
+          <h4 className="text-xl font-semibold">Contacto</h4>
+          <ul className="space-y-2">
+            <li className="flex items-center gap-2">
+              <Phone size={16} />
+              <span className="text-lg text-gray-light">
+                +61 3 8376 6284 <br /> +44 3 7890 - 123
+              </span>
+            </li>
+            <li className="flex items-center gap-2">
+              <Mail size={16} />
+              <span className="text-lg text-gray-light">
+                bswebstudios@gmail.com
+              </span>
+            </li>
+            <li className="flex items-center gap-2">
+              <MapPin size={16} />
+              <span className="text-lg text-gray-light">Iquique, Chile</span>
+            </li>
+          </ul>
+        </div>
+
+        {/* col 4 follow us*/}
+        <div className="space-y-4 md:mx-auto">
+          <h4 className="text-xl font-semibold">Síguenos</h4>
+          <div className="flex gap-4">
+            <a
+              href="https://www.facebook.com/ElLokojara"
+              aria-label="Facebook"
+              className="text-white hover:text-gray-light"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </a>
+            <a
+              href="https://www.instagram.com/blackshark.studios/"
+              aria-label="Instagram"
+              className="text-white hover:text-gray-light"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+          </div>
         </div>
       </div>
+
+      {/* line below, copyright*/}
+      <div className="mt-12 pt-8 border-t border-gray-800 text-center text-gray-light">
+        <p>
+          &copy; {new Date().getFullYear()} Black Shark Studios. Todos los
+          derechos reservados.
+        </p>
+      </div>
     </footer>
-  )
-}
+  );
+};
