81375c78150651ece01fa4893e36f39b2fdbb19d
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2025-02-20 18:52:17 -0300

    fix: resolve merge conflicts in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import tailwindcssAnimate from "tailwindcss-animate";
-=======
-import tailwindcssAnimate from 'tailwindcss-animate'
->>>>>>> 3cf58cc0a63818e0c6f9919524958afaa8b6b75a
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -92,8 +88,4 @@ export default {
     },
   },
   plugins: [tailwindcssAnimate],
-<<<<<<< HEAD
-};
-=======
 }
->>>>>>> 3cf58cc0a63818e0c6f9919524958afaa8b6b75a
